4e5aed8f6e0632c29abc80adc4b2fd5bfea3d480
virgiliusnanamanek <84966287+virgiliusnanamanek@users.noreply.github.com>
2022-06-03 10:21:46 +0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+
+function CounterDisplay({ count }) {
+  return <p>{count}</p>;
+}
+
+function IncraseButton({ increase }) {
+  return (
+    <div>
+      <button onClick={increase}>+ increase</button>
+    </div>
+  );
+}
+
+function ResetButton({ reset }) {
+  return (
+    <div>
+      <button onClick={reset}>- reset</button>
+    </div>
+  );
+}
+
+class CounterApp extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0
+    };
+
+    this.onIncreaseEventHandler = this.onIncreaseEventHandler.bind(this);
+    this.onResetEventHandler = this.onResetEventHandler.bind(this);
+  }
+  onIncreaseEventHandler() {
+    this.setState((previousState) => {
+      return {
+        count: previousState.count + 1
+      };
+    });
+  }
+  onResetEventHandler() {
+    this.setState(() => {
+      return {
+        count: 0
+      };
+    });
+  }
+  render() {
+    return (
+      <div>
+        <IncraseButton increase={this.onIncreaseEventHandler} />
+        <CounterDisplay count={this.state.count} />
+        <ResetButton reset={this.onResetEventHandler} />
+      </div>
+    );
+  }
+}
+
+const root = createRoot(document.getElementById('root'));
+root.render(<CounterApp />);
